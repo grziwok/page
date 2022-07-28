d00c50cb03aa7f2edcf7d5dc7c4663194fc5f015
Krzysztof Grziwok <grziwok@users.noreply.github.com>
2022-07-28 23:02:46 +0200

    fix: Change apostrophe to for linter

--- src/components/about/about.component.tsx
+++ src/components/about/about.component.tsx
@@ -3,6 +3,6 @@ import * as S from './about.styles';
 
 export const About: React.FC = () => (
     <S.About>
-        Hello! I'm Krzysztof Grziwok - software developer with focus on the details. Currently Senior Front-End Developer working in Betsys, where we are developing the largest betting platform in country.
+        Hello! I&apos;m Krzysztof Grziwok - software developer with focus on the details. Currently Senior Front-End Developer working in Betsys, where we are developing the largest betting platform in country.
     </S.About>
 );
