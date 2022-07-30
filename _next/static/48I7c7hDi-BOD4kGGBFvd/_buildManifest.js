cfe0dbdd05c0360e1ca829ed25676e1165fa1e3b
grziwok <grziwok@users.noreply.github.com>
2022-07-30 14:20:29 +0000

    Deploying to public from @ grziwok/page@77459c2875103e8261559207c4dd276d1c0e0ff3 🚀

---
+++ _next/static/48I7c7hDi-BOD4kGGBFvd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/217-bbfc6d9ff7157fe9.js","static/chunks/pages/index-02b48bac4a728463.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- _next/static/OBxbZ7wEDV7fbPDhYTO9G/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/217-bbfc6d9ff7157fe9.js","static/chunks/pages/index-06fcf222ca95f40a.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
