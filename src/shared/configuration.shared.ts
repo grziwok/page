2eed387b8526805e4d87f01c0ab5ccf7e8e899d0
Krzysztof Grziwok <grziwok@users.noreply.github.com>
2022-07-31 11:25:21 +0200

    fix: Render styles in server side rendering

--- src/shared/configuration.shared.ts
+++ src/shared/configuration.shared.ts
@@ -1,21 +1,21 @@
 export const CONFIGURATION = {
-  title: 'Krzysztof Grziwok',
-  description: 'Personal page by Krzysztof Grziwok',
-  projects: [
-      {
-          name: 'STS',
-          description: 'Mobile version of application for the largest betting company in Poland',
-      },
-      {
-          name: 'cux.io',
-          description: 'Startup for detecting user behavior in your applications',
-      },
-  ],
-  footer: {
-      links: [
-          { name: 'Resume', href: '/resume' },
-          { name: 'github.com/grziwok', href: 'https://github.com/grziwok', newTab: true },
-          { name: 'linkedin.com/in/grziwok', href: 'https://www.linkedin.com/in/grziwok/', newTab: true },
-      ],
-  },
+    title: 'Krzysztof Grziwok',
+    description: 'Personal page by Krzysztof Grziwok',
+    projects: [
+        {
+            name: 'STS',
+            description: 'Mobile version of application for the largest betting company in Poland',
+        },
+        {
+            name: 'cux.io',
+            description: 'Startup for detecting user behavior in your applications',
+        },
+    ],
+    footer: {
+        links: [
+            { name: 'Resume', href: '/resume' },
+            { name: 'github.com/grziwok', href: 'https://github.com/grziwok', newTab: true },
+            { name: 'linkedin.com/in/grziwok', href: 'https://www.linkedin.com/in/grziwok/', newTab: true },
+        ],
+    },
 };
