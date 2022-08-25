4159eaf4c1e687051d42dd2ec50fdae7ee4cc66a
grziwok <grziwok@users.noreply.github.com>
2022-08-25 20:42:27 +0000

    Deploying to public from @ grziwok/page@29192cd049861e04c857f4086dc12d5757915a0d 🚀

---
+++ _next/static/j5fXkVe7c_iIj0dLFbiHA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/217-6b2a9151196d19b7.js","static/chunks/pages/index-4cfdb28921ef9c1e.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
